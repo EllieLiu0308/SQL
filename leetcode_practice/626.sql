7c4a8911f9945e5cd5de95e180e65bb7a2bc3251
EllieLiu0308 <148903764+EllieLiu0308@users.noreply.github.com>
2024-07-10 22:11:01 -0400

    626

---
+++ leetcode_practice/626.sql
@@ -0,0 +1,7 @@
+select case 
+      when id%2 = 1 and id< (select max(id) from seat) then id +1
+      when id%2 = 1 and id= (select max(id) from seat) then id  
+      else id -1
+      end as id, student
+from seat
+order by id asc
